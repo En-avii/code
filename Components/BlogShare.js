97410ee17bddd57d526f787db1fa25ce5e582b81
Abhishek Yadav <27abhiyadav@gmail.com>
2024-11-18 22:31:04 +0530

    Update BlogShare.js

--- Components/BlogShare.js
+++ Components/BlogShare.js
@@ -1,24 +1,22 @@
-import { FaTwitter } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 
 function BlogShare({ data }) {
   return (
     <>
       <div className="text-center pb-4">
-        <button className="bg-indigo-500 px-3 py-1 font-semibold text-white inline-flex items-center space-x-2 rounded">
-          <FaTwitter />
+        <button className="bg-blue-600 px-3 py-1 font-semibold text-white inline-flex items-center space-x-2 rounded">
+          <FaLinkedin />
           <a
-            className="twitter-share-button"
+            className="linkedin-share-button"
             rel="noopener noreferrer"
             target="_blank"
-            href={`https://twitter.com/intent/tweet?text=${
-              data.Title
-            } by @Abhishek
-            &url=blogs.Abhishek/blogs/${String(
-              data.Title.split(" ").join("-").toLowerCase()
-            )}
-            &hashtags=${data.Tags.split(" ")}`}
+            href={`https://www.linkedin.com/sharing/share-offsite/?url=${
+              encodeURIComponent(`https://blogs.Abhishek/blogs/${String(
+                data.Title.split(" ").join("-").toLowerCase()
+              )}`)
+            }`}
           >
-            Tweet
+            Share
           </a>
         </button>
       </div>
